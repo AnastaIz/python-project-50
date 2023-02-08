fafeea005c830c7f6100aea3f90c965ae4c1e185
Anastasia Izmaylova <abulatova1992@gmail.com>
2023-02-08 14:14:39 +0300

    fix path to files

--- tests/test_flat_json.py
+++ tests/test_flat_json.py
@@ -2,7 +2,7 @@ from gendiff import generate_diff
 
 
 def test_flat_json():
-    diff_file1_file2 = generate_diff('/Users/anastasiaizmaylova/python-project-50/tests/fixtures/file1.json',
-                                    '/Users/anastasiaizmaylova/python-project-50/tests/fixtures/file2.json')
-    correct_diff = open('/Users/anastasiaizmaylova/python-project-50/tests/fixtures/flat_json.txt')
+    diff_file1_file2 = generate_diff('tests/fixtures/file1.json',
+                                    'tests/fixtures/file2.json')
+    correct_diff = open('tests/fixtures/flat_json.txt')
     assert diff_file1_file2 == correct_diff.read()
