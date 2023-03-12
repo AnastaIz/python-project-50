5da8c192727d21e4bfd75b31d41d636c39901f25
Anastasia Izmaylova <abulatova1992@gmail.com>
2023-03-12 20:36:37 +0300

    add test for diff function

---
+++ tests/test_diff.py
@@ -0,0 +1,15 @@
+from gendiff.data_parser import open_file
+from gendiff.diff import diff
+
+
+def test_diff():
+    result = {'follow': {'type': 'removed', 'value': False},
+              'host': {'type': 'unchanged', 'value': 'hexlet.io'},
+              'proxy': {'type': 'removed', 'value': '123.234.53.22'},
+              'timeout': {'type': 'updated', 'value1': 50, 'value2': 20},
+              'verbose': {'type': 'added', 'value': True}}
+
+    data1 = open_file('tests/fixtures/file1.json')
+    data2 = open_file('tests/fixtures/file2.json')
+
+    assert diff(data1, data2) == result
