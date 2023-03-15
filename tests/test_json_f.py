ca1f3fb2907979cc1ead390c34e6c64402d3dbda
Anastasia Izmaylova <abulatova1992@gmail.com>
2023-03-15 14:50:00 +0300

    add test for json formatter

---
+++ tests/test_json_f.py
@@ -0,0 +1,14 @@
+import json
+from gendiff.diff import diff
+from gendiff.json_f import json_
+from gendiff.data_parser import open_file
+
+
+def test_json_f():
+    result = diff(
+        open_file('tests/fixtures/file1.json'),
+        open_file('tests/fixtures/file2.json')
+    )
+    to_json = json_(result)
+
+    assert json.loads(to_json) == result
