b842d6886ff46c06f2f73ad3cbf4ae1a775e3705
Anastasia Izmaylova <abulatova1992@gmail.com>
2023-03-15 14:48:29 +0300

    fix flake8 errors in cli.py

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -7,8 +7,8 @@ def parse_args_cli():
     )
     parser.add_argument("first_file")
     parser.add_argument("second_file")
-    parser.add_argument("-f", "--format", 
-                        default='stylish', 
-                        choices=['stylish', 'plain', 'json'], 
+    parser.add_argument("-f", "--format",
+                        default='stylish',
+                        choices=['stylish', 'plain', 'json'],
                         help="set format of output")
     return parser.parse_args()
