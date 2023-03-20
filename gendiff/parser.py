9944e1e9600d22559f6e55e40f152a3773259639
Anastasia Izmaylova <abulatova1992@gmail.com>
2023-03-20 17:24:23 +0300

    add parser

---
+++ gendiff/parser.py
@@ -0,0 +1,11 @@
+import json
+import yaml
+
+
+def parse(data, type_format):
+    if type_format == 'json':
+        return json.loads(data)
+    elif type_format == 'yaml':
+        return yaml.safe_load(data)
+    else:
+        raise ValueError("Invalid file format")
